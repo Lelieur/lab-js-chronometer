df6eb04db94f540078b6193a4d177ec263e89a56
Lucas Lelieur <lucas.lelieur.ll@gmail.com>
2024-10-24 17:40:07 +0200

    Lab solved with Bonus Iteration 9 pending

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,87 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+
+      if (printTimeCallback) {
+
+        printTimeCallback()
+      }
+    }, 1000)
+
+
   }
 
   getMinutes() {
     // ... your code goes here
+
+    let minutes = Math.floor(this.currentTime / 60)
+
+    return minutes
+
+
   }
 
   getSeconds() {
     // ... your code goes here
+
+    if (!this.currentTime) {
+      return 0
+    }
+
+    let seconds = this.currentTime - this.getMinutes() * 60
+
+
+    return seconds
+
+
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+
+
+    if (!value) {
+      return '00'
+    }
+
+    if (value >= 10) {
+      return (`${value}`)
+    }
+
+    if (value > 0 && value < 10) {
+      return (`0${value}`)
+    }
+
+
+
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
     // ... your code goes here
+
+    const currentMinutes = this.computeTwoDigitNumber(this.getMinutes())
+    const currentSeconds = this.computeTwoDigitNumber(this.getSeconds())
+
+    return `${currentMinutes}:${currentSeconds}`
+
   }
 }
